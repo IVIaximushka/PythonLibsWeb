6707215f3b984664f8e0d0cf0fbea8def94b4b36
Дмитриев Максим Сергеевич <DmitrievMS@abdt.ru>
2025-02-06 16:45:03 +0300

    add admin

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from web.models import Institute, Speciality, StudyPlan, Discipline
+
+
+class InstituteAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "school", "is_active")
+    search_fields = ("id", "name")
+    list_filter = ("is_active", "school")
+
+
+class SpecialityAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "institute", "is_active")
+    search_fields = ("id", "name", "institute__name")
+    list_filter = ("is_active", "institute__name")
+
+
+class StudyPlanAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "speciality", "is_active")
+    search_fields = ("id", "name", "speciality__name")
+    list_filter = ("is_active", "speciality__institute__name", "speciality__name")
+
+
+class DisciplineAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "is_active")
+    search_fields = ("id", "name")
+    list_filter = ("is_active",)
+
+
+admin.site.register(Institute, InstituteAdmin)
+admin.site.register(Speciality, SpecialityAdmin)
+admin.site.register(StudyPlan, StudyPlanAdmin)
+admin.site.register(Discipline, DisciplineAdmin)
--- web/models.py
+++ web/models.py
@@ -5,30 +5,58 @@ User = get_user_model()
 
 
 class Institute(models.Model):
-    name = models.CharField(max_length=100)
-    school = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
+    name = models.CharField(max_length=100, verbose_name="Название института")
+    school = models.BooleanField(default=False, verbose_name="Является подразделением института")
+    is_active = models.BooleanField(default=True, verbose_name="Активен")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "институт"
+        verbose_name_plural = "институты"
 
 
 class Speciality(models.Model):
-    name = models.CharField(max_length=200)
-    institute = models.ForeignKey(Institute, on_delete=models.CASCADE)
-    is_active = models.BooleanField(default=True)
+    name = models.CharField(max_length=200, verbose_name="Название специальности")
+    institute = models.ForeignKey(Institute, on_delete=models.CASCADE, verbose_name="Институт")
+    is_active = models.BooleanField(default=True, verbose_name="Активен")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "специальность"
+        verbose_name_plural = "специальности"
 
 
 class StudyPlan(models.Model):
-    name = models.CharField(max_length=200)
-    speciality = models.ForeignKey(Speciality, on_delete=models.CASCADE)
-    is_active = models.BooleanField(default=True)
+    name = models.CharField(max_length=200, verbose_name="Название учебного плана")
+    speciality = models.ForeignKey(Speciality, on_delete=models.CASCADE, verbose_name="Специальность")
+    is_active = models.BooleanField(default=True, verbose_name="Активен")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "учебный план"
+        verbose_name_plural = "учебные планы"
 
 
 class Discipline(models.Model):
-    name = models.CharField(max_length=200)
-    is_active = models.BooleanField(default=True)
+    name = models.CharField(max_length=200, verbose_name="Название дисциплины")
+    is_active = models.BooleanField(default=True, verbose_name="Активен")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "дисциплина"
+        verbose_name_plural = "дисциплины"
 
 
 class StudyPlanDiscipline(models.Model):
-    is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True, verbose_name="Активен")
     course = models.SmallIntegerField()
     code = models.CharField(max_length=100)
     semester = models.SmallIntegerField()
@@ -38,5 +66,5 @@ class StudyPlanDiscipline(models.Model):
     practice = models.IntegerField()
     lab = models.IntegerField()
     by_choice = models.BooleanField(default=False)
-    study_plan = models.ForeignKey(StudyPlan, on_delete=models.CASCADE)
-    discipline = models.ForeignKey(Discipline, on_delete=models.CASCADE)
+    study_plan = models.ForeignKey(StudyPlan, on_delete=models.CASCADE, verbose_name="Учебный план")
+    discipline = models.ForeignKey(Discipline, on_delete=models.CASCADE, verbose_name="Дисциплина")
